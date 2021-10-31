b737cd161d041c23b0e55c169b28bb694c8ced7e
Денис Гришкевич <cris12@mail.ru>
2021-10-31 17:18:39 +0300

    add new tests

--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -7,22 +7,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
 
     @Test
+    void increaseVolumeMaxFinish() {
+        Radio volume = new Radio();
+        int expected = 10;
+        int actual = volume.increaseVolume(10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void increaseVolume() {
-            Radio volume = new Radio();
-            int expected = 10;
-            int actual = volume.increaseVolume(10);
-            assertEquals(expected, actual);
-        }
+        Radio volume = new Radio();
+        int expected = 9;
+        int actual = volume.increaseVolume(8);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseVolumeMaxRelevant() {
+        Radio volume = new Radio();
+        int expected = 10;
+        int actual = volume.increaseVolume(9);
+        assertEquals(expected, actual);
+    }
 
     @Test
     void reduceVolume() {
         Radio volume = new Radio();
-        int expected = 0;
-        int actual = volume.reduceVolume(  0);
+        int expected = 4;
+        int actual = volume.reduceVolume(5);
         assertEquals(expected, actual);
     }
+
     @Test
-    void increaseStation() {
+    void reduceVolumeMinFinish() {
+        Radio volume = new Radio();
+        int expected = 0;
+        int actual = volume.reduceVolume(0);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void reduceVolumeMinRelevant() {
+        Radio volume = new Radio();
+        int expected = 0;
+        int actual = volume.reduceVolume(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseStationReturn0() {
         Radio volume = new Radio();
         int expected = 0;
         int actual = volume.increaseStation(9);
@@ -30,11 +64,28 @@ class RadioTest {
     }
 
     @Test
-    void reduceStation() {
+    void increaseStation() {
         Radio volume = new Radio();
         int expected = 9;
-        int actual = volume.reduceStation(  0);
+        int actual = volume.increaseStation(8);
         assertEquals(expected, actual);
     }
+
+
+    @Test
+    void reduceStation() {
+        Radio volume = new Radio();
+        int expected = 7;
+        int actual = volume.reduceStation(8);
+        assertEquals(expected, actual);
     }
+
+    @Test
+    void reduceStationReturn9() {
+        Radio volume = new Radio();
+        int expected = 9;
+        int actual = volume.reduceStation(0);
+        assertEquals(expected, actual);
+    }
+}
 
