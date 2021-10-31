1c54d90e91285c81dd6f77ab29f7d084a2c063b7
Денис Гришкевич <cris12@mail.ru>
2021-10-31 17:05:11 +0300

    tests complite

--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -10,30 +10,30 @@ class RadioTest {
     void increaseVolume() {
             Radio volume = new Radio();
             int expected = 10;
-            int actual = volume.increaseVolume(9);
+            int actual = volume.increaseVolume(10);
             assertEquals(expected, actual);
         }
 
     @Test
     void reduceVolume() {
         Radio volume = new Radio();
-        int expected = 2;
-        int actual = volume.reduceVolume(  3);
+        int expected = 0;
+        int actual = volume.reduceVolume(  0);
         assertEquals(expected, actual);
     }
     @Test
     void increaseStation() {
         Radio volume = new Radio();
-        int expected = 8;
-        int actual = volume.increaseStation(7);
+        int expected = 0;
+        int actual = volume.increaseStation(9);
         assertEquals(expected, actual);
     }
 
     @Test
     void reduceStation() {
         Radio volume = new Radio();
-        int expected = 3;
-        int actual = volume.reduceStation(  4);
+        int expected = 9;
+        int actual = volume.reduceStation(  0);
         assertEquals(expected, actual);
     }
     }
